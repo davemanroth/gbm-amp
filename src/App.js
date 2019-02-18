6a247edaa1c1a9fe8880a435a557b54009b6232f
Dave Rothfarb <dave.rothfarb.webdev@gmail.com>
2019-02-18 12:09:52 -0500

    Building GeneIdInput, in process of parsing and storing entered text

--- src/App.js
+++ src/App.js
@@ -5,20 +5,13 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.getIds = this.getIds.bind(this);
-    this.state = {}
   }
-
-  getIds = () = {
-  }
-
 
   render() {
     return (
       <div className="container">
         <h1>Glioblastoma Multiforme Amplifications</h1>
         <GeneIdInput 
-          geneIds={ this.getIds }
         />
       </div>
     );
--- src/components/GeneIdInput.js
+++ src/components/GeneIdInput.js
@@ -3,14 +3,45 @@ import React, { Component } from 'react';
 class GeneIdInput extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.processIds = this.processIds.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { 
+      input: "",
+      ids: [] 
+    }
+  }
+
+  processIds = () => {
+    const idsToStore = [];
+    let ids = this.state.input;
+    ids = ids.split(" ");
+    ids.map( (id) => {
+      const num = Number.parseInt(id);
+      if( this.isNum(num)) {
+        idsToStore.push(num);
+      }
+    }
+    this.setState({ ids: idsToStore });
+  }
+
+  handleChange = (e) => {
+    this.setState({ input: e.target.value });
   }
 
   render = () => {
     return (
       <div className="form-group">
-        <label htmlFor="gene-input">Please input gene ID(s), separated by spaces or commas</label>
-        <textarea className="form-control" rows="3"></textarea>
+        <label htmlFor="gene-input">Please input gene ID(s), separated by spaces</label>
+        <textarea 
+          onChange={ this.handleChange }
+          className="form-control" 
+          rows="3">
+        </textarea>
+        <button 
+          role="button" 
+          className="btn btn-primary btn-lg"
+          onClick={ this.processIds }
+        >Graph gene IDs</button>
       </div>
     );
   }
